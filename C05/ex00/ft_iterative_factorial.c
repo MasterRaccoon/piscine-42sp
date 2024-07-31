0b1ae3fb718ab071e5b0e362614f57e1ce07b1e7
MasterRaccoon <davi@pop-os.localdomain>
2024-07-31 16:31:48 -0300

    C05/ex00 ok

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb == 0)
+	{
+		return (1);
+	}
+	result = 1;
+	while (nb > 1)
+	{
+		result = result * nb;
+		nb--;
+	}
+	return (result);
+}
+/*
+int	main(void)
+{
+	int	number;
+
+	number = -1;
+	printf("%d", ft_iterative_factorial(number));
+	printf("\n");
+	number = 0;
+	printf("%d", ft_iterative_factorial(number));
+	printf("\n");
+	number = 5;
+	printf("%d", ft_iterative_factorial(number));
+	printf("\n");
+	number = 6;
+	printf("%d", ft_iterative_factorial(number));
+	printf("\n");
+	number = 7;
+	printf("%d", ft_iterative_factorial(number));
+	printf("\n");
+}
+*/
